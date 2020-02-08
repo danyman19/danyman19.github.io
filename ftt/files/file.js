697161f1404a617e343dd765780b36f5bae316cd
litblock <37122962+litblock@users.noreply.github.com>
2020-02-07 20:33:06 -0500

    Add files via upload

---
+++ ftt/files/file.js
@@ -0,0 +1,26 @@
+//You have found the flag
+//Or did you?
+//ftt{th1s_1s_n0t_th3_fl4g_l0l}
+//f
+//t
+//t
+//{
+//w
+//4
+//s
+//_
+//t
+//h
+//4
+//t
+//_
+//h
+//4
+//r
+//d
+//3
+//r
+//?
+//}
+//I got ya
+//You got pranked :)
